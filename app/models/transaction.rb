2b9fa33a0fe9c48f6337be561901186a82c8a2cc
Stephen Freund <sdfreund10@gmail.com>
2018-04-18 21:11:19 -0400

    Lint transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,17 @@ class Transaction < ApplicationRecord
   belongs_to :account
   validates :account, presence: true
   validates_uniqueness_of :description,
-                          scope: %i(account_id posting_date balance)
+                          scope: %i[account_id posting_date balance]
+
+  def self.upsert(params)
+    params.symbolize_keys!
+    if params[:posting_date].present?
+      params[:posting_date] = Date.strptime(params[:posting_date], '%m/%d/%Y')
+    end
+    find_or_initialize_by(
+      params.slice(:description, :account_id, :posting_date, :balance)
+    ).update!(
+      params
+    )
+  end
 end

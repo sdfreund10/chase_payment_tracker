7e035f4c7269f1530edf927a0501175600145d98
Stephen Freund <sdfreund10@gmail.com>
2018-04-22 23:17:33 -0400

    Sets up react
    Setup react and initialize homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def index; end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])

2cddbf0a659cd6b1f0518b8493abf1de11b29d64
Stephen Freund <sdfreund10@gmail.com>
2018-05-10 21:41:42 -0400

    Sets up feature tests with rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,24 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
+require "selenium/webdriver"
+
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
